7a8db152ca8cbc92446245004dea902b1256a8d4
GeneralMobile <you@example.com>
2022-02-08 22:43:55 +0100

    upload days app

---
+++ html.js
@@ -0,0 +1,145 @@
+const getCSS = () => {
+
+    return `
+    @font-face {
+        font-family: 'digital-7';
+        src: url('../fonts/digital-7.ttf');
+      }
+      
+      body {
+        color: #ffffff;
+        background-color: #000000;
+        font-family: 'digital-7', sans-serif;
+      }
+      
+      /* DAYS OF THE WEEK */
+      
+      div.days {
+        margin: 0 auto;
+        color: #131212;
+      }
+      
+      div.days .day { display: inline-block; }
+      
+      div.days .day p {
+        font-size: 12px;
+        font-weight: bold;
+        font-family: sans-serif;
+        text-transform: uppercase;
+      }
+      
+      /* CLOCK */
+      
+      div.clock { margin: 5px 0; }
+      
+      div.clock div {
+        display: inline-block;
+        position: relative;
+      }
+      
+      div.clock div p {
+        font-size: 100px;
+        position: relative;
+        z-index: 100;
+      }
+      
+      div.clock .placeholder {
+        color: #131212;
+        position: absolute;
+        top: 0;
+        z-index: 50;
+      }
+      
+      div.clock .meridian { margin-left: 15px; }/*END CLOCK*/
+      
+      /* AM OR PM*/
+      
+      .am-pm {
+        font-family: sans-serif;
+        text-transform: uppercase;
+        width: 20px;
+      }
+      
+      div.am-pm div p {
+        font-size: 12px;
+        font-weight: bold;
+        width: 100%;
+      }
+      
+      .am, .pm { color: #131212; }
+      
+      /*CLASS THAT CHANGES COLOR OF TEXT TO APPEAR LIKE ITS "ON"*/
+      
+      .light-on { color: #ffffff; }
+
+
+      .fullDays {
+       color: #03DAC6
+      }
+
+      .fullDays.am {
+        #03DAC6
+      }
+
+      .workingDays {
+        color: #018786
+       }
+ 
+      .workingDays.pm {
+         #018786
+      }
+      `
+}
+
+const getHTML = (oVal) => {
+    const iFullDays = oVal.fullDays,
+        iWorkingDays = oVal.workdaysLeftExcluding;
+    return `
+    <html>
+    <head>
+        <style>
+            ${getCSS()}
+        </style>
+    </head>
+    <body>
+    <div>
+        <div class="clock">
+            <!-- HOUR -->
+            <div class="numbers">
+                <p class="fullDays">${iFullDays}</p>
+                <p class="placeholder"></p>
+            </div>
+        
+            <div class="colon">
+                <p>/</p>
+            </div>
+        
+            <!-- MINUTE -->
+            <div class="numbers">
+                <p class="workingDays">${iWorkingDays}</p>
+                <p class="placeholder"></p>
+            </div>
+        
+            <!-- AM / PM -->
+            <div class="am-pm">
+        
+            <!-- AM -->
+            <div class="">
+                <p class="fullDays am">fulldays</p>
+            </div>
+        
+            <!-- PM -->
+            <div>
+                <p class="workingDays pm">working days</p>
+            </div>
+        </div>
+    </div>
+    </body>
+    </html>
+    `
+};
+
+
+module.exports = {
+    getHTML: getHTML
+}
--- index.js
+++ index.js
@@ -1,11 +1,87 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
+const html = require("./html.js");
+
+/**
+ * Calculates the full working days until the end of march from now on
+ * @returns 
+ */
+var getWorkingDaysTillEndOfMarch = () => {
+
+    function daysInMonth(iMonth, iYear) {
+        return 32 - new Date(iYear, iMonth, 32).getDate();
+    }
+
+    function isWeekday(year, month, day) {
+        var day = new Date(year, month, day).getDay();
+        return day != 0 && day != 6;
+    }
+
+    function getWeekdaysInMonth(month, year) {
+        var days = daysInMonth(month, year);
+        var weekdays = 0;
+        for (var i = 0; i < days; i++) {
+            if (isWeekday(year, month, i + 1)) weekdays++;
+        }
+        return weekdays;
+    }
+
+    liCurrentMonth = new Date().getMonth();
+    liCurrentDayInMonth = new Date().getDate();
+    liCurrentYear = new Date().getFullYear();
+    liDaysInMonth = daysInMonth(liCurrentMonth, liCurrentYear);
+
+    var liTotalWorkdays = 0;
+    var liMonthCounter = liCurrentMonth;
+    while (liMonthCounter < 3) {
+        if (liCurrentDayInMonth > 0) {
+
+            var isWeekDay = isWeekday(liCurrentYear, liCurrentMonth, liCurrentDayInMonth++);
+            if (isWeekDay) {
+                liTotalWorkdays++;
+            }
+            if (liCurrentDayInMonth === liDaysInMonth) {
+                liCurrentDayInMonth = 0;
+                liMonthCounter++;
+            }
+        } else {
+            liTotalWorkdays = liTotalWorkdays + getWeekdaysInMonth(liMonthCounter, liCurrentYear);
+            liMonthCounter++;
+        }
+
+    }
+
+    return liTotalWorkdays;
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
+    let today = new Date();
+
+    var iEndMonth = 2;
+    var iMonth = today.getMonth();
+
+    var iYear = today.getFullYear();
+
+    var oFullDays = new Date(iYear, iEndMonth, 30);
+
+
+    if (iMonth == 11 && today.getDate() > 25) {
+        oFullDays.setFullYear(oFullDays.getFullYear() + 1);
+    }
+    var one_day = 1000 * 60 * 60 * 24;
+
+    var iFullDays = Math.ceil((oFullDays.getTime() - today.getTime()) / (one_day));
+
+    var oResult = {
+        fullDays: iFullDays,
+        workdaysLeftExcluding: getWorkingDaysTillEndOfMarch()
+    };
+    console.log(oResult);
+    res.send(html.getHTML(oResult));
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
